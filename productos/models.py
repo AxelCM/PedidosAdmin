8f844e430430b6b0d4a8e046488c6dd10e30d6fa
Axel Castillo <axel-cas@hotmail.com>
2019-03-25 23:28:22 -0600

    creando tercero

--- productos/models.py
+++ productos/models.py
@@ -3,12 +3,9 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField('nombre' , max_length=100 )
-<<<<<<< HEAD
     precio = models.IntegerField('Precio' , default=0 )
-=======
     segundo = models.CharField('claro' , max_length=50)
-
->>>>>>> e7461ce890620ecc40d61ef5ff20dc43a22ac49d
+    tercero =models.CharField('tercero' , max_length=50)
 
     def __str__ (self):
         return "%s" % (self.nombre)
