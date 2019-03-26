e7461ce890620ecc40d61ef5ff20dc43a22ac49d
oasamayoa <oasamayoa14@gmail.com>
2019-03-25 23:15:25 -0600

    actualizando repo

--- productos/models.py
+++ productos/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField('nombre' , max_length=100 )
+    segundo = models.CharField('claro' , max_length=50)
 
 
     def __str__ (self):
