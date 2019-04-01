9969adbf298c75f2e77031a207d261a676088841
Axel Castillo <axel-cas@hotmail.com>
2019-03-31 19:11:32 -0600

    Se agrega vistas para agregar a los clientes y poder editar su informacion

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from clientes.views import Register
+from clientes.views import Register , ClientesView  , ClienteDetailView , UpdateClient
 
 urlpatterns = [
     path('registrar/cliente/' , Register.as_view() , name='register_client'),
+    path('detail/clientes/<int:id_cliente>/' , ClienteDetailView.as_view() , name='detail_client'),
+    path('ver/clientes/' , ClientesView.as_view() , name='list_client'),
+    path('actualizar/cliente/<int:pk>/' , UpdateClient.as_view() , name='update_client')
+    #path('editar/cliente/<int:pk>/' , UpdateClient.as_view() , name='update_client'),
 
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse ,reverse_lazy
-from django.views.generic import FormView
+from django.views.generic import FormView , TemplateView , UpdateView , DetailView
 from clientes.forms import RegisterClientForm
 
 from clientes.models import Cliente
@@ -17,3 +17,31 @@ class Register(FormView):
         """Save form data."""
         form.save()
         return super().form_valid(form)
+
+class ClienteDetailView(DetailView):
+    template_name = 'clientes/client_detail.html'
+    slug_field = 'id_cliente'
+    slug_url_kwarg = 'id_cliente'
+    queryset = Cliente.objects.all()
+    context_object_name = 'cliente'
+
+class ClientesView(TemplateView):
+    template_name = 'clientes/client_list.html'
+
+    def get_context_data(self, *args , **kwargs):
+        clientes = Cliente.objects.all()
+        return {'clientes' : clientes}
+
+
+class UpdateClient(UpdateView):
+    model = Cliente
+    fields = ['n_representante',
+            'nombre_comercial',
+            'telefono',
+            'direccion',
+            'mayorista',
+            'email',
+            'nit',
+        ]
+    success_url = reverse_lazy('list_client')
+    template_name = 'clientes/form_update_cliente.html'
